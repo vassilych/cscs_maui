06b91996e5fed22e46112a67c3f5e072195a03b1
Vassili Kaplan <vassilik@gmail.com>
2024-08-26 22:13:46 +0200

    Platforms

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ScriptingMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
